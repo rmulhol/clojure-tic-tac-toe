91b78b768a71718292d615ef9232ce74f75e2271
Robert Mulholand <robmulholand@gmail.com>
2015-05-19 15:38:43 -0500

    add rules

---
+++ src/tic_tac_toe/rules.clj
@@ -0,0 +1,28 @@
+(ns tic-tac-toe.rules
+  (:require [tic-tac-toe.board :as Board]))
+
+(defn winning-combinations [board]
+  (concat (Board/rows board) (Board/columns board) (Board/diagonals board)))
+
+(defn player-controls-combination? [move-signature combo board]
+  (loop [amount-of-combo-claimed 0
+         remaining-elements combo]
+    (cond
+      (= amount-of-combo-claimed (count combo)) true
+      (not (= move-signature (board (first remaining-elements)))) false
+      :else (recur (inc amount-of-combo-claimed) (rest remaining-elements)))))
+
+(defn player-wins? [move-signature board]
+  (loop [remaining-combinations (winning-combinations board)]
+    (cond
+      (empty? remaining-combinations) false
+      (player-controls-combination? 
+        move-signature (first remaining-combinations) board) true
+      :else (recur (rest remaining-combinations)))))
+
+(defn game-over? [board player-1 player-2]
+  (cond
+    (player-wins? player-1 board) true
+    (player-wins? player-2 board) true
+    (Board/board-full? board) true
+    :else false))
