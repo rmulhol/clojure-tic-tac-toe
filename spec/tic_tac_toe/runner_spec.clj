5f17a8514a0aaf4b7e24ac89008220e6c7217bee
Robert Mulholand <robmulholand@gmail.com>
2015-05-26 17:15:33 -0500

    use multimethod for runner polymorphism on take-turn

--- spec/tic_tac_toe/runner_spec.clj
+++ spec/tic_tac_toe/runner_spec.clj
@@ -2,6 +2,36 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toe.runner :as runner]))
 
+(describe "identify-player-type"
+  (it "identifies an ai player"
+    (should= :ai 
+             (runner/identify-player-type 
+               [" " " " " " " " " " " " " " " " " "]
+               { :identity :ai, :move-signature "X" })))
+
+  (it "identifies a human player"
+    (should= :human 
+             (runner/identify-player-type 
+               [" " " " " " " " " " " " " " " " " "]
+               { :identity :human, :move-signature "X"}))))
+
+(describe "take-turn"
+  (around [it]
+    (with-out-str (it)))
+
+  (it "takes a turn for an ai player"
+    (should= ["X" "O" " " " " "O" " " " " " " "X"]
+             (runner/take-turn
+               ["X" " " " " " " "O" " " " " " " "X"]
+               { :identity :ai, :move-signature "O" })))
+
+  (it "takes a turn for a human player"
+    (should= ["X" " " " " " " " " " " " " " " " "]
+             (with-in-str "1"
+               (runner/take-turn
+                 [" " " " " " " " " " " " " " " " " "]
+                 { :identity :human, :move-signature "X" })))))
+
 (describe "play"
   (it "runs through a HvC game where computer wins"
     (should-contain "Player O wins!"
@@ -12,3 +42,4 @@
     (should-contain "Tie game!"
       (with-out-str (with-in-str "human\nX\nai\nO\n1\n9\n8\n3\n4\n"
         (runner/play))))))
+
--- src/tic_tac_toe/runner.clj
+++ src/tic_tac_toe/runner.clj
@@ -7,6 +7,23 @@
             [tic-tac-toe.board-presenter :as bp]
             [tic-tac-toe.config :as config]))
 
+(defn identify-player-type [board current-player]
+  (current-player :identity))
+
+(defmulti take-turn identify-player-type)
+
+(defmethod take-turn :ai [board current-player]
+  (board/place-move 
+    board 
+    (current-player :move-signature) 
+    (ai/get-move (current-player :move-signature) board)))
+
+(defmethod take-turn :human [board current-player]
+  (board/place-move
+    board
+    (current-player :move-signature)
+    (io/get-move board)))
+
 (defn play []
   (io/output (messages/game-intro))
   (let [player-1 (config/create-player "1" "")
@@ -15,25 +32,12 @@
            current-player player-1
            opponent player-2]
       (io/output (bp/display board))
-      (cond
-        (rules/game-over? board (player-1 :move-signature) (player-2 :move-signature))
-          (io/announce-result 
-            board 
-            (player-1 :move-signature) 
-            (player-2 :move-signature))
-        (= :ai (current-player :identity))
-          (recur 
-            (board/place-move 
-              board 
-              (current-player :move-signature) 
-              (ai/get-move (current-player :move-signature) board)) 
-            opponent 
-            current-player)
-        :else 
-          (recur 
-            (board/place-move 
-              board 
-              (current-player :move-signature) 
-              (io/get-move board)) 
-            opponent 
-            current-player)))))
+      (if (rules/game-over? board (player-1 :move-signature) (player-2 :move-signature))
+        (io/announce-result 
+          board 
+          (player-1 :move-signature) 
+          (player-2 :move-signature))
+        (recur 
+          (take-turn board current-player)
+          opponent
+          current-player)))))
